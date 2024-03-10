1b4f2b098124650421974aff7cb0c2ee26286378
piaoger <piaoger@yahoo.com>
2024-03-10 15:48:21 +0800

    chore:minor cleanups

--- benches/bench.rs
+++ benches/bench.rs
@@ -167,30 +167,6 @@ fn bench_script_embedded(c: &mut Criterion) {
     #[cfg(any(feature = "bench_javascript", feature = "bench_lite_javascript"))]
     group.bench_function("js_boa_eval_fn", |b| {
         let mut context = boa_engine::Context::default();
-
-        // // We register a global closure function that has the name 'closure' with length 0.
-        // context
-        //     .register_global_callable(
-        //         "log",
-        //         1,
-        //         boa_engine::NativeFunction::from_copy_closure(
-        //         |_this, args, _context | {
-        //             if args.len() == 0 {
-        //                 return Ok(boa_engine::JsValue::Undefined);
-        //             }
-        //
-        //             if let Ok(arg) = args[0].to_string( _context) {
-        //                 println!("log: {}", arg.to_std_string().unwrap());
-        //                 Ok(boa_engine::JsValue::Undefined)
-        //             } else {
-        //                 Ok(boa_engine::JsValue::Undefined)
-        //             }
-        //         }
-        //         ),
-        //     )
-        //     .unwrap();
-
-
         context.eval(boa_engine::Source::from_bytes(CRC_JS)).unwrap();
         let mut i = 0_f64;
         b.iter(|| {
